1743076eab23cf65d4e9b5555fe6fe9534b69023
maiconm <maiconm23@gmail.com>
2020-11-28 16:21:46 -0300

    Editar e excluir elementos da lista

--- lib/pages/lista_tarefas_page.dart
+++ lib/pages/lista_tarefas_page.dart
@@ -1,5 +1,6 @@
 import 'package:cadastro_tarefas/model/tarefa.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class ListaTarefasPage extends StatefulWidget {
   @override
@@ -63,9 +64,9 @@ class _ListaTarefasPageState extends State<ListaTarefasPage> {
           itemBuilder: _criarItensMenuPopup,
           onSelected: (String valorSelecionado) {
             if (valorSelecionado == ACAO_EDITAR) {
-              _abrirForm();
+              _abrirForm(tarefaAtual: tarefa, indice: index);
             } else {
-              _excluir();
+              _excluir(index);
             }
           }
         );
@@ -89,7 +90,7 @@ class _ListaTarefasPageState extends State<ListaTarefasPage> {
         ),
       ),
       PopupMenuItem(
-        value: ACAO_EDITAR,
+        value: ACAO_EXCLUIR,
         child: Row(
           children: [
             Icon(Icons.delete, color: Colors.red),
@@ -103,15 +104,171 @@ class _ListaTarefasPageState extends State<ListaTarefasPage> {
     ];
   }
 
-  void _abrirForm() {
-    // TODO
+  void _abrirForm({Tarefa tarefaAtual, int indice}) {
+    final key = GlobalKey<_ConteudoFormDialogState>();
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(tarefaAtual == null ? 'Nova Tarefa' : 'Alterar Tarefa (${tarefaAtual.id})'),
+          content: ConteudoFormDialog(
+            key: key,
+            tarefaAtual: tarefaAtual,
+          ),
+          actions: [
+            FlatButton(
+              child: Text('Cancelar'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            FlatButton(
+              onPressed: () {
+                if (key.currentState.dadosValidos()) {
+                  setState(() {
+                    final novaTarefa = key.currentState.novaTarefa;
+                    if (indice == null) {
+                      novaTarefa.id = ++_ultimoId;
+                      _tarefas.add(novaTarefa);
+                    } else {
+                      _tarefas[indice] = novaTarefa;
+                    }
+                  });
+                  Navigator.of(context).pop();
+                }
+              },
+              child: Text('Salvar'),
+            ),
+          ],
+        );
+      }
+    );
   }
 
-  void _excluir() {
-    // TODO
+  void _excluir(int indice) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Row(
+            children: [
+              Icon(Icons.warning),
+              Padding(
+                padding: EdgeInsets.only(left: 10),
+                child: Text('Atenção'),
+              ),
+            ],
+          ),
+          content: Text('Esse registro será removido definitivamente.'),
+          actions: [
+            FlatButton(
+              child: Text('Cancelar'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            FlatButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+                setState(() {
+                  _tarefas.removeAt(indice);
+                });
+              },
+            ),
+          ],
+        );
+      }
+    );
   }
 
   void _abrirPaginaFiltro() {
     // TODO
   }
+}
+
+class ConteudoFormDialog extends StatefulWidget {
+  final Tarefa tarefaAtual;
+
+  ConteudoFormDialog({Key key, this.tarefaAtual}): super(key: key);
+
+  @override
+  _ConteudoFormDialogState createState() => _ConteudoFormDialogState();
+}
+
+class _ConteudoFormDialogState extends State<ConteudoFormDialog> {
+  final _formKey = GlobalKey<FormState>();
+  final _descricaoController = TextEditingController();
+  final _prazoController = TextEditingController();
+  final _dateFormat = DateFormat('dd/MM/yyyy');
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.tarefaAtual != null) {
+      _descricaoController.text = widget.tarefaAtual.descricao;
+      _prazoController.text = widget.tarefaAtual.prazoFormatado;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          TextFormField(
+            controller: _descricaoController,
+            decoration: InputDecoration(labelText: 'Descrição',),
+            validator: (String valor) {
+              if (valor == null || valor.isEmpty) {
+                return 'Informe a descrição';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            controller: _prazoController,
+            decoration: InputDecoration(
+              labelText: 'Prazo',
+                prefixIcon: IconButton(
+                icon: Icon(Icons.calendar_today),
+                onPressed: _mostrarCalendario,
+              ),
+              suffixIcon: IconButton(
+                icon: Icon(Icons.close),
+                onPressed: () => _prazoController.clear(),
+              ),
+            ),
+            readOnly: true,
+          ),
+        ],
+      ),
+    );
+  }
+
+  _mostrarCalendario() async {
+    final dataFormatada = _prazoController.text;
+    var agora = DateTime.now();
+    if (dataFormatada.isNotEmpty) {
+      agora = _dateFormat.parse(dataFormatada);
+    }
+    final dataSelecionada = await showDatePicker(
+      context: context,
+      initialDate: agora,
+      firstDate: agora.subtract(Duration(days: 5 * 365)),
+      lastDate: agora.add(Duration(days: 5 * 365)),
+    );
+    if (dataSelecionada != null) {
+      setState(() {
+        _prazoController.text = _dateFormat.format(dataSelecionada);
+      });
+    }
+  }
+
+  bool dadosValidos() => _formKey.currentState.validate();
+
+  Tarefa get novaTarefa => Tarefa(
+    id: widget.tarefaAtual?.id,
+    descricao: _descricaoController.text,
+    prazo: _prazoController.text.isEmpty ? null : _dateFormat.parse(_prazoController.text),
+  );
+
 }
